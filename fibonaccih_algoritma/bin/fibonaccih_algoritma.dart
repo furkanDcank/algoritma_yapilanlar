831da7ebe5c94a92b7173ae4c49afe9d4cfae490
furkanDcank <139819441+furkanDcank@users.noreply.github.com>
2023-08-27 22:34:47 +0300

    new commit

---
+++ fibonaccih_algoritma/bin/fibonaccih_algoritma.dart
@@ -0,0 +1,5 @@
+import 'package:fibonaccih_algoritma/fibonaccih_algoritma.dart' as fibonaccih_algoritma;
+
+void main(List<String> arguments) {
+  print('Hello world: ${fibonaccih_algoritma.calculate()}!');
+}
---
+++ fibonaccih_algoritma/test/fibonaccih_algoritma_test.dart
@@ -0,0 +1,8 @@
+import 'package:fibonaccih_algoritma/fibonaccih_algoritma.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
