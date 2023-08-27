1e4502c131465c8cacd9d9fbc7f425e216ed46d0
furkanDcank <139819441+furkanDcank@users.noreply.github.com>
2023-08-28 01:45:07 +0300

    last_change

--- fibonaccih_algoritma/bin/fibonaccih_algoritma.dart
+++ fibonaccih_algoritma/bin/fibonaccih_algoritma.dart
@@ -1,5 +1,18 @@
-import 'package:fibonaccih_algoritma/fibonaccih_algoritma.dart' as fibonaccih_algoritma;
-
 void main(List<String> arguments) {
-  print('Hello world: ${fibonaccih_algoritma.calculate()}!');
+
+  fibonaccihCalculator(7);
+
+}
+void fibonaccihCalculator(int value){
+  int oldValue = 0;
+  int newValue = 1;
+  int reslut;
+  for(int i = 1; i <= value; i++) {
+    print('index : $i');
+    print('sayac : $oldValue + $newValue');
+    reslut = oldValue + newValue;
+    oldValue = newValue;
+    newValue = reslut;
+    print(reslut);
+  }
 }
